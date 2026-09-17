3f8ff02ba3674a3159663cd29f22e4506d3e88c5
Carol Xu <xucarol.20@gmail.com>
2026-09-17 12:54:02 -0400

    ⋆.°⋆~₍ᐢ. ༝ .ᐢ₎~⋆.°⋆

---
+++ analysis/analysis.R
@@ -0,0 +1,9 @@
+## Preliminaries -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggthemes, readxl, data.table, gdata, ipumsr)
+
+# Set working directory 
+setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/noncitzens medicaid")
+
+# ACS data -----------------------------------------------------------------
+acs = readRDS("data/output/acs.RDS")
